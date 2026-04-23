51eefc16b48e1e4d6bf7dd79d4ebbcee0b0318d8
Daniel Nata Nugraha <daniel.nata@flower.ai>
2026-04-23 09:18:02 +0200

    Rename dir

--- framework/py/flwr/supercore/executors/__init__.py
+++ framework/py/flwr/supercore/executors/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower AgentApp components."""
+"""Flower Executors components."""
 
 
 from .run_agentapp import run_agentapp
