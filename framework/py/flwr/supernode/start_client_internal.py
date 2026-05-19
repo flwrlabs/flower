bce5a7f68079f9535fb3b03a5f15575c5864a304
Heng Pan <pan@flower.ai>
2026-05-19 17:10:30 +0800

    init

--- framework/py/flwr/supernode/start_client_internal.py
+++ framework/py/flwr/supernode/start_client_internal.py
@@ -46,6 +46,7 @@ from flwr.common.constant import (
     TRANSPORT_TYPES,
     ErrorCode,
     ExecPluginType,
+    Status,
     SubStatus,
 )
 from flwr.common.exit import ExitCode, flwr_exit, register_signal_handlers
@@ -491,6 +492,8 @@ def _push_messages(
     # This is to ensure that only one message is processed at a time
     # Wait until a reply message is available
     while not (reply_messages := state.get_messages(is_reply=True)):
+        # Temporary: Trigger cleanup of expired task tokens
+        state.get_tasks(statuses=[Status.PENDING])
         time.sleep(0.5)
 
     for message in reply_messages:
