8c0f76335e33475344e6f77eeff7c560cb321f0d
Daniel J. Beutel <daniel@flower.ai>
2026-05-28 15:05:33 +0200

    refactor(framework): Move ServerApp to flwr.serverapp

--- framework/py/flwr/simulation/run_simulation.py
+++ framework/py/flwr/simulation/run_simulation.py
@@ -42,7 +42,7 @@ from flwr.common.typing import Run
 from flwr.proto.task_pb2 import Task  # pylint: disable=E0611
 from flwr.server.grid import Grid, InMemoryGrid
 from flwr.server.run_serverapp import run as _run
-from flwr.server.server_app import ServerApp
+from flwr.serverapp import ServerApp
 from flwr.server.superlink.fleet import vce
 from flwr.server.superlink.fleet.vce.backend.backend import BackendConfig
 from flwr.server.superlink.linkstate import InMemoryLinkState, LinkStateFactory
