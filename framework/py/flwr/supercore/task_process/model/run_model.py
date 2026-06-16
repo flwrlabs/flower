d0ebc2599c960d95e20092866b70383cc93c11a6
Daniel Nata Nugraha <daniel.nata@flower.ai>
2026-06-16 11:48:35 +0200

    Format

--- framework/py/flwr/supercore/task_process/model/run_model.py
+++ framework/py/flwr/supercore/task_process/model/run_model.py
@@ -47,7 +47,7 @@ from flwr.supercore.interceptors import (
 from .task import handle_task
 
 
-def run_model(
+def run_model(  # pylint: disable=too-many-locals
     serverappio_api_address: str,
     token: str,
     insecure: bool,
