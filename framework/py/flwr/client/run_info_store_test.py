e351cf3ad35076250a1217ed2f97f952722dc49c
Daniel J. Beutel <daniel@flower.ai>
2026-06-21 17:18:44 +0200

    Rename test

--- framework/py/flwr/client/run_info_store_test.py
+++ framework/py/flwr/client/run_info_store_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Node state tests."""
+"""Run info store tests."""
 
 
 from typing import cast
@@ -32,7 +32,7 @@ def _run_dummy_task(context: Context) -> Context:
     return context
 
 
-def test_multirun_in_node_state() -> None:
+def test_multirun_in_run_info_store() -> None:
     """Test basic DeprecatedRunInfoStore logic."""
     # Tasks to perform
     run_ids = [0, 1, 1, 2, 3, 2, 1, 5]
