56951f30fab3b982ec44d6921129c74e6ceac8fa
Heng Pan <pan@flower.ai>
2026-06-23 10:54:08 +0100

    Apply suggestion from @panh99

--- framework/py/flwr/supercore/task_process/connector/browser_use/__init__.py
+++ framework/py/flwr/supercore/task_process/connector/browser_use/__init__.py
@@ -19,6 +19,7 @@ import os
 
 from flwr.supercore.typing import JSONObject
 
+# Prevent browser-use from configuring its own logging, which would duplicate Flower logs
 os.environ["BROWSER_USE_SETUP_LOGGING"] = "false"
 
 try:
