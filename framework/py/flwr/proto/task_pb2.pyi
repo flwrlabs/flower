c6c367b8a14d1d609102a3dacbd7a2bb03a8ef29
Daniel Nata Nugraha <daniel.nata@flower.ai>
2026-07-02 14:15:43 +0200

    Add usage_type

--- framework/py/flwr/proto/task_pb2.pyi
+++ framework/py/flwr/proto/task_pb2.pyi
@@ -138,18 +138,21 @@ class TaskUsage(google.protobuf.message.Message):
     INPUT_TOKENS_FIELD_NUMBER: builtins.int
     OUTPUT_TOKENS_FIELD_NUMBER: builtins.int
     TOTAL_TOKENS_FIELD_NUMBER: builtins.int
+    USAGE_TYPE_FIELD_NUMBER: builtins.int
     input_tokens: builtins.int
     output_tokens: builtins.int
     total_tokens: builtins.int
+    usage_type: builtins.str
     def __init__(
         self,
         *,
         input_tokens: builtins.int | None = ...,
         output_tokens: builtins.int | None = ...,
         total_tokens: builtins.int | None = ...,
+        usage_type: builtins.str = ...,
     ) -> None: ...
     def HasField(self, field_name: typing.Literal["_input_tokens", b"_input_tokens", "_output_tokens", b"_output_tokens", "_total_tokens", b"_total_tokens", "input_tokens", b"input_tokens", "output_tokens", b"output_tokens", "total_tokens", b"total_tokens"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing.Literal["_input_tokens", b"_input_tokens", "_output_tokens", b"_output_tokens", "_total_tokens", b"_total_tokens", "input_tokens", b"input_tokens", "output_tokens", b"output_tokens", "total_tokens", b"total_tokens"]) -> None: ...
+    def ClearField(self, field_name: typing.Literal["_input_tokens", b"_input_tokens", "_output_tokens", b"_output_tokens", "_total_tokens", b"_total_tokens", "input_tokens", b"input_tokens", "output_tokens", b"output_tokens", "total_tokens", b"total_tokens", "usage_type", b"usage_type"]) -> None: ...
     @typing.overload
     def WhichOneof(self, oneof_group: typing.Literal["_input_tokens", b"_input_tokens"]) -> typing.Literal["input_tokens"] | None: ...
     @typing.overload
