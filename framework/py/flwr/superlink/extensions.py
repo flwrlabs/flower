4217310af318b8e0f012211cc19817e13beca15c
mohammadnaseri <mohammad@flower.ai>
2026-07-06 16:47:11 +0100

    Edit

--- framework/py/flwr/superlink/extensions.py
+++ framework/py/flwr/superlink/extensions.py
@@ -32,11 +32,9 @@ SuperLinkLifespanContext = Callable[
 class _SuperLinkExtensions(Protocol):
     def configure_app(self, app: FastAPI) -> None:
         """Configure the FastAPI app."""
-        pass
 
     def get_lifespan_contexts(self) -> tuple[SuperLinkLifespanContext, ...]:
         """Return lifespan contexts."""
-        pass
 
 
 def _get_ee_extensions() -> _SuperLinkExtensions | None:
