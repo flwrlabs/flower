575b5fa8d9a8563ce8aeab39715f0577e2b6f6f0
jafermarq <javier@flower.ai>
2026-07-14 18:09:10 +0100

    w/ prev

--- framework/py/flwr/supernode/main.py
+++ framework/py/flwr/supernode/main.py
@@ -25,6 +25,7 @@ from fastapi import FastAPI
 
 from flwr import __version__
 from flwr.common import log
+from flwr.supercore.error import http_error_translator
 from flwr.supercore.routers import health
 from flwr.supernode.routers import runtime
 
@@ -51,6 +52,8 @@ def create_app() -> FastAPI:
 
     # SuperNode APIs
     fastapi_app.include_router(runtime.router)
+
+    fastapi_app.middleware("http")(http_error_translator)
 
     return fastapi_app
 
