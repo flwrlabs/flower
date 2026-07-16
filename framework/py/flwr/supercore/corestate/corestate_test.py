2873ba02060832ca0a3b95f4fdd0c6e028a1811b
Heng Pan <pan@flower.ai>
2026-07-16 10:07:49 +0100

    format

--- framework/py/flwr/supercore/corestate/corestate_test.py
+++ framework/py/flwr/supercore/corestate/corestate_test.py
@@ -364,8 +364,9 @@ class StateTest(unittest.TestCase):  # pylint: disable=R0904
         with (
             patch("flwr.supercore.date.datetime.datetime") as mock_datetime,
             patch.object(
-                # pylint: disable-next=W0212
-                state, "_cleanup_push_session", wraps=state._cleanup_push_session
+                state,
+                "_cleanup_push_session",
+                wraps=state._cleanup_push_session,  # pylint: disable=W0212
             ) as cleanup_session,
         ):
             mock_datetime.now.return_value = expired_at
