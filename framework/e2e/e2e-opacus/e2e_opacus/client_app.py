ebccc97dfdb0653cacf55ad85ee4b230434c267a
Taner Topal <taner@adap.com>
2026-08-11 15:37:06 +0200

    Further reduce E2E workload

--- framework/e2e/e2e-opacus/e2e_opacus/client_app.py
+++ framework/e2e/e2e-opacus/e2e_opacus/client_app.py
@@ -48,17 +48,17 @@ class Cifar10Dataset(torch.utils.data.Dataset):
 class Net(nn.Module):
     def __init__(self) -> None:
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 4, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(4, 8, 5)
+        self.fc1 = nn.Linear(8 * 5 * 5, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 10)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 8 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
