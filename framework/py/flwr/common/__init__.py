98e2d73a47e1763a37d57953f82fb446ebd8e597
mohammadnaseri <mohammad@flower.ai>
2026-08-19 16:31:31 +0100

    Edit

--- framework/py/flwr/common/__init__.py
+++ framework/py/flwr/common/__init__.py
@@ -55,11 +55,11 @@ from ..compat.common.typing import Status as Status
 from ..compat.common.typing import Value as Value
 from ..supercore.date import now as now
 from ..supercore.grpc import GRPC_MAX_MESSAGE_LENGTH as GRPC_MAX_MESSAGE_LENGTH
-from ..supercore.logger import configure as configure
-from ..supercore.logger import log as log
 from ..supercore.telemetry import EventType as EventType
 from ..supercore.telemetry import event as event
 from .constant import MessageTypeLegacy as MessageTypeLegacy
+from .logger import configure as configure
+from .logger import log as log
 
 if TYPE_CHECKING:
     from ..app.message import Array as Array
