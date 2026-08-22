28755cc0b7962e6a22fd9aef040980b2eb669ecb
Daniel Nata Nugraha <daniel.nata@flower.ai>
2026-08-22 16:07:47 +0200

    Format

--- framework/py/flwr/cli/chat/chat_app.py
+++ framework/py/flwr/cli/chat/chat_app.py
@@ -98,8 +98,8 @@ from flwr.supercore.constant import (
 from flwr.supercore.typing import JSONObject
 
 from ..auth_plugin import CliAuthPlugin, OidcCliPlugin
-from .chat_transcript import MarkdownBlock, render_markdown
 from ..utils import flwr_cli_grpc_exc_handler
+from .chat_transcript import MarkdownBlock, render_markdown
 
 
 @dataclass
