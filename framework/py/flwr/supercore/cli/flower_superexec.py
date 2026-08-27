ebed1492b1c9a9e5d63ad45877c8c01d3fbff3f1
jafermarq <javier@flower.ai>
2026-08-27 09:32:17 +0100

    exact flag name

--- framework/py/flwr/supercore/cli/flower_superexec.py
+++ framework/py/flwr/supercore/cli/flower_superexec.py
@@ -58,10 +58,7 @@ def flower_superexec() -> None:
     warn_if_flwr_update_available(process_name="flower-superexec")
     args = _parse_args().parse_args()
 
-    if any(
-        arg == "--appio-api-address" or arg.startswith("--appio-api-address=")
-        for arg in sys.argv[1:]
-    ):
+    if "--appio-api-address" in sys.argv[1:]:
         log(
             WARN,
             "The `--appio-api-address` argument has been renamed to "
