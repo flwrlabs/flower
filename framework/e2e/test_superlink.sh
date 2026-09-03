5ce78adc888839334f3a4eead4f94d5b8cc6987a
jafermarq <javier@flower.ai>
2026-09-03 10:05:40 +0100

    E2E use http

--- framework/e2e/test_superlink.sh
+++ framework/e2e/test_superlink.sh
@@ -63,10 +63,10 @@ sed -i '/^\[tool\.flwr\.federations\.e2e\]/,/^$/d' pyproject.toml
 # Check if the first argument is 'insecure'
 if [ "$server_arg" = "--insecure" ]; then
   # If $server_arg is '--insecure', append the first line
-  echo -e $"\n[tool.flwr.federations.e2e]\naddress = \"127.0.0.1:9093\"\ninsecure = true" >> pyproject.toml
+  echo -e $"\n[tool.flwr.federations.e2e]\naddress = \"127.0.0.1:8000\"\ninsecure = true" >> pyproject.toml
 else
   # Otherwise, append the second line
-  echo -e $"\n[tool.flwr.federations.e2e]\naddress = \"127.0.0.1:9093\"\nroot-certificates = \"certificates/ca.crt\"" >> pyproject.toml
+  echo -e $"\n[tool.flwr.federations.e2e]\naddress = \"127.0.0.1:8000\"\nroot-certificates = \"certificates/ca.crt\"" >> pyproject.toml
 fi
 
 background_pids=()
